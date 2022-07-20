e91eda5ba3b8eabff494a101becb1afca2f8f3f6
Eric Charles <badass@stanford.edu>
2022-07-20 15:34:33 -0700

    Remove scons stuff

--- examples/script.sh
+++ examples/script.sh
@@ -13,40 +13,24 @@ ${command} create
 ${command} insert --level production --production_name ${p_name} --handler ${handler} --config_yaml ${config}
 ${command} insert --recurse --level campaign --production_name ${p_name} --campaign_name ${c_name} --handler ${handler} --config_yaml ${config} --butler_repo ${butler_repo}
 
-${command} prepare --recurse --level step --production_name ${p_name} --campaign_name ${c_name} --step_name step1
+${command} prepare --level step --production_name ${p_name} --campaign_name ${c_name} --step_name step1
 ${command} queue --level step --production_name ${p_name} --campaign_name ${c_name} --step_name step1
 ${command} launch --level step --production_name ${p_name} --campaign_name ${c_name} --step_name step1
 ${command} fake_run --level step --production_name ${p_name} --campaign_name ${c_name} --step_name step1
-${command} check --level workflow --production_name ${p_name} --campaign_name ${c_name} --step_name step1
-${command} accept --level workflow --production_name ${p_name} --campaign_name ${c_name} --step_name step1
-${command} check --level group --production_name ${p_name} --campaign_name ${c_name} --step_name step1
-${command} accept --level group --production_name ${p_name} --campaign_name ${c_name} --step_name step1
-${command} check --level step --production_name ${p_name} --campaign_name ${c_name} --step_name step1
 ${command} accept --level step --production_name ${p_name} --campaign_name ${c_name} --step_name step1
 
-${command} prepare --recurse --level step --production_name ${p_name} --campaign_name ${c_name} --step_name step2
+${command} prepare --level step --production_name ${p_name} --campaign_name ${c_name} --step_name step2
 ${command} queue --level step --production_name ${p_name} --campaign_name ${c_name} --step_name step2
 ${command} launch --level step --production_name ${p_name} --campaign_name ${c_name} --step_name step2
 ${command} fake_run --level step --production_name ${p_name} --campaign_name ${c_name} --step_name step2
-${command} check --level workflow --production_name ${p_name} --campaign_name ${c_name} --step_name step2
-${command} accept --level workflow --production_name ${p_name} --campaign_name ${c_name} --step_name step2
-${command} check --level group --production_name ${p_name} --campaign_name ${c_name} --step_name step2
-${command} accept --level group --production_name ${p_name} --campaign_name ${c_name} --step_name step2
-${command} check --level step --production_name ${p_name} --campaign_name ${c_name} --step_name step2
 ${command} accept --level step --production_name ${p_name} --campaign_name ${c_name} --step_name step2
 
-${command} prepare --recurse --level step --production_name ${p_name} --campaign_name ${c_name} --step_name step3
+${command} prepare --level step --production_name ${p_name} --campaign_name ${c_name} --step_name step3
 ${command} queue --level step --production_name ${p_name} --campaign_name ${c_name} --step_name step3
 ${command} launch --level step --production_name ${p_name} --campaign_name ${c_name} --step_name step3
 ${command} fake_run --level step --production_name ${p_name} --campaign_name ${c_name} --step_name step3
-${command} check --level workflow --production_name ${p_name} --campaign_name ${c_name} --step_name step3
-${command} accept --level workflow --production_name ${p_name} --campaign_name ${c_name} --step_name step3
-${command} check --level group --production_name ${p_name} --campaign_name ${c_name} --step_name step3
-${command} accept --level group --production_name ${p_name} --campaign_name ${c_name} --step_name step3
-${command} check --level step --production_name ${p_name} --campaign_name ${c_name} --step_name step3
 ${command} accept --level step --production_name ${p_name} --campaign_name ${c_name} --step_name step3
 
-${command} check --level campaign --production_name ${p_name} --campaign_name ${c_name}
 ${command} accept --level campaign --production_name ${p_name} --campaign_name ${c_name}
 ${command} accept --recurse --level step --production_name ${p_name} --campaign_name ${c_name} --step_name step1
 
