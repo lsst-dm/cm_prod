fb5773df93f05776d8c7bb10f67afb253c9a12b4
Eric Charles <echarles@gmail.com>
2022-07-22 17:26:31 -0700

    update example

--- examples/script.sh
+++ examples/script.sh
@@ -14,28 +14,9 @@ ${command} insert --level campaign --production_name ${p_name} --campaign_name $
 ${command} prepare --level campaign --production_name ${p_name} --campaign_name ${c_name}
 
 ${command} prepare --level step --production_name ${p_name} --campaign_name ${c_name} --step_name step1
-#${command} queue --level step --production_name ${p_name} --campaign_name ${c_name} --step_name step1
-#${command} launch --level step --production_name ${p_name} --campaign_name ${c_name} --step_name step1
-#${command} fake_run --level step --production_name ${p_name} --campaign_name ${c_name} --step_name step1
-#${command} accept --level step --production_name ${p_name} --campaign_name ${c_name} --step_name step1#
-
-#${command} prepare --level step --production_name ${p_name} --campaign_name ${c_name} --step_name step2
-#${command} queue --level step --production_name ${p_name} --campaign_name ${c_name} --step_name step2
-#${command} launch --level step --production_name ${p_name} --campaign_name ${c_name} --step_name step2
-#${command} fake_run --level step --production_name ${p_name} --campaign_name ${c_name} --step_name step2
-#${command} accept --level step --production_name ${p_name} --campaign_name ${c_name} --step_name step2
-
-#${command} prepare --level step --production_name ${p_name} --campaign_name ${c_name} --step_name step3
-#${command} queue --level step --production_name ${p_name} --campaign_name ${c_name} --step_name step3
-#${command} launch --level step --production_name ${p_name} --campaign_name ${c_name} --step_name step3
-#${command} fake_run --level step --production_name ${p_name} --campaign_name ${c_name} --step_name step3
-#${command} accept --level step --production_name ${p_name} --campaign_name ${c_name} --step_name step3
-
-#${command} accept --level campaign --production_name ${p_name} --campaign_name ${c_name}
-#${command} accept --recurse --level step --production_name ${p_name} --campaign_name ${c_name} --step_name step1
 
 ${command} print_table --level production
 ${command} print_table --level campaign
 ${command} print_table --level step
-#${command} print_table --level group
-#${command} print_table --level workflow
+${command} print_table --level group
+${command} print_table --level workflow
