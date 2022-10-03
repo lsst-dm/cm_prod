22d8ca6208e7721499cb44a37d14e1ef7d5c32ca
Eric Charles <echarles@gmail.com>
2022-10-03 16:26:18 -0700

    use panda setup script

--- src/lsst/cm/prod/configs/HSC/test/hsc_bps_template.sh
+++ src/lsst/cm/prod/configs/HSC/test/hsc_bps_template.sh
@@ -1,17 +1,11 @@
 #!/bin/sh
 
-export WEEKLY='2022_36'
-source /cvmfs/sw.lsst.eu/linux-x86_64/lsst_distrib/w_${WEEKLY}/loadLSST.bash
+# setup LSST env.
+export WEEKLY='w_2022_40'
+source /cvmfs/sw.lsst.eu/linux-x86_64/lsst_distrib/${WEEKLY}/loadLSST.bash
 setup lsst_distrib
 
-# setup PanDA env. Will be a simple step when the deployment of PanDA is fully done.
-export PANDA_CONFIG_ROOT=$HOME
-export PANDA_URL_SSL=https://pandaserver-doma.cern.ch:25443/server/panda
-export PANDA_URL=http://pandaserver-doma.cern.ch:25080/server/panda
-export PANDA_AUTH=oidc
-export PANDA_VERIFY_HOST=off
-export PANDA_AUTH_VO=Rubin
-
-# IDDS_CONFIG path depends on the weekly version
-idds_cfg_template=/cvmfs/sw.lsst.eu/linux-x86_64/lsst_distrib/w_${WEEKLY}/conda/envs/lsst-scipipe-*/etc/idds/idds.cfg.client.template
-export IDDS_CONFIG=`echo $idds_cfg_template |  cut -d ' ' -f1`
+# setup PanDA env.
+latest_panda=$(ls -td /cvmfs/sw.lsst.eu/linux-x86_64/panda_env/v* | head -1)
+setupScript=${latest_panda}/setup_panda_s3df.sh
+source $setupScript ${WEEKLY}
