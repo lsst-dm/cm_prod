e057d69f2972a43b7a697191b408c90c86a175f0
Eric Charles <echarles@gmail.com>
2023-02-15 13:17:29 -0800

    Fix logic for checker

--- src/lsst/cm/prod/HSC/handler.py
+++ src/lsst/cm/prod/HSC/handler.py
@@ -1,7 +1,10 @@
 from lsst.cm.tools.core.panda_utils import PandaChecker
+from lsst.cm.tools.core.utils import ScriptMethod
 from lsst.cm.tools.db.job_handler import JobHandler
 
 
 class HSCJobHandler(JobHandler):
 
     checker_class_name = PandaChecker().get_checker_class_name()
+
+    JobHandler.checker_class_dict[ScriptMethod.slurm] = PandaChecker
