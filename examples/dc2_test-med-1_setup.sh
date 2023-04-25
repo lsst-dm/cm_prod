9743ac1398a98b9dd39d6b11d5837ec0d35cc6e2
Orion Eiger <quasardragon96@gmail.com>
2023-04-25 12:02:50 -0700

    Added text for test workflow

--- examples/dc2_test-med-1_setup.sh
+++ examples/dc2_test-med-1_setup.sh
@@ -8,7 +8,7 @@ config="dc2_test-med-1.yaml"
 config_name="dc2"
 p_name="dc2-test-med-1"
 lsst_version="w_2023_13"
-c_name="${lsst_version}"
+c_name="${lsst_version}_test"
 fullname="${p_name}/${c_name}"
 db_path="output/cm_${p_name}_${c_name}.db"
 
