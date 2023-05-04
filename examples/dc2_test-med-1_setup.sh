c824acace20c82e39c26b6e4819a076ef1c00657
Orion Eiger <quasardragon96@gmail.com>
2023-05-04 11:30:40 -0700

    some added memory for some tasks; also a one-line fix for the configs so that steps can run without specific dataqueries

--- examples/dc2_test-med-1_setup.sh
+++ examples/dc2_test-med-1_setup.sh
@@ -9,7 +9,7 @@ config="dc2_test-med-1.yaml"
 config_name="dc2"
 p_name="dc2-test-med-1"
 lsst_version="w_2023_13"
-c_name="${lsst_version}_test"
+c_name="${lsst_version}_test1"
 fullname="${p_name}/${c_name}"
 db_path="output/cm_${p_name}_${c_name}.db"
 
