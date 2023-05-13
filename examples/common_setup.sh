f2f1170614df759f1685e49afdebc8a83187f118
Orion Eiger <quasardragon96@gmail.com>
2023-05-12 17:31:25 -0700

    One line fix to get the cm_prod directory starting in the same place

--- examples/common_setup.sh
+++ examples/common_setup.sh
@@ -5,7 +5,7 @@ EXAMPLES=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 butler_repo="/sdf/group/rubin/repo/main"
 root_coll="u/${USER}/cm"
 
-export CM_PROD_DIR="${EXAMPLES}"
+export CM_PROD_DIR=`echo ${EXAMPLES} | sed 's/\/examples//'`
 export CM_PROD_URL="output/archive"
 export CM_BUTLER="${butler_repo}"
 export CM_SCRIPT_METHOD="slurm"
