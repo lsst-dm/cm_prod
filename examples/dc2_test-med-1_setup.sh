85a0702885d8a8617717bfe07fec18b696f51765
Sierra Villarreal <sierrav@slac.stanford.edu>
2023-06-07 11:24:53 -0700

    modified configs for successful running on USDF

--- examples/dc2_test-med-1_setup.sh
+++ examples/dc2_test-med-1_setup.sh
@@ -9,7 +9,7 @@ config="dc2_test-med-1.yaml"
 config_name="dc2"
 p_name="dc2-test-med-1"
 lsst_version="w_2023_17"
-c_name="${lsst_version}_test2"
+c_name="${lsst_version}_test3"
 fullname="${p_name}/${c_name}"
 db_path="output/cm_${p_name}_${c_name}.db"
 
