663d510f1fa39616226d8d312e0d64a14adcf73f
Orion Eiger <eiger@slac.stanford.edu>
2023-09-28 17:20:54 -0700

    Everything needed for w_2023_35 HSC-RC2 run

--- examples/hsc_weekly_setup.sh
+++ examples/hsc_weekly_setup.sh
@@ -6,9 +6,10 @@ source $EXAMPLES/common_setup.sh
 
 config="hsc_weekly.yaml"
 config_name="hsc_weekly"
-p_name="HSC"
-lsst_version="w_2023_08"
-c_name="${lsst_version}_test0"
+ticket_num="DM-40588"
+lsst_version="w_2023_35"
+p_name="${lsst_version}"
+c_name="${ticket_num}"
 fullname="${p_name}/${c_name}"
 db_path="output/cm_${p_name}.db"
 
