2411c1f5af272d13979921531eccf15472b6ea4b
Orion Eiger <eiger@slac.stanford.edu>
2023-10-20 15:20:36 -0700

    Setup for DC2 test-med-1 with w_2023_41

--- examples/dc2_test-med-1_setup.sh
+++ examples/dc2_test-med-1_setup.sh
@@ -6,9 +6,10 @@ source $EXAMPLES/common_setup.sh
 
 config="dc2_test-med-1.yaml"
 config_name="dc2"
-p_name="dc2-test-med-1"
-lsst_version="w_2023_17"
-c_name="${lsst_version}_test3"
+ticket_num='DM-41286'
+lsst_version="w_2023_41"
+p_name="${lsst_version}"
+c_name="${ticket_num}"
 fullname="${p_name}/${c_name}"
 db_path="output/cm_${p_name}_${c_name}.db"
 
--- src/lsst/cm/prod/configs/DC2/test/dc2_bps_template.sh
+++ src/lsst/cm/prod/configs/DC2/test/dc2_bps_template.sh
@@ -11,8 +11,8 @@ setup lsst_distrib
 
 # setup PanDA env.
 latest_panda=$(ls -td /cvmfs/sw.lsst.eu/linux-x86_64/panda_env/v* | head -1)
-source ${latest_panda}/setup_lsst.sh ${WEEKLY}
-source ${latest_panda}/setup_panda.sh
+setupScript=${latest_panda}/setup_panda_s3df.sh
+source $setupScript ${WEEKLY}
 
 env | grep PANDA
 
