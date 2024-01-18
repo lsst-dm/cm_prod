6047bc128b22e839b5792dd405baf9da37ce3513
Orion Eiger <eiger@slac.stanford.edu>
2024-01-18 15:13:26 -0800

    Add comment about stack setup

--- src/lsst/cm/prod/configs/HSC/test/hsc_bps_template.sh
+++ src/lsst/cm/prod/configs/HSC/test/hsc_bps_template.sh
@@ -4,12 +4,13 @@
 # the lsst python _after_ the virtual env python in the PATH, which
 # is causing errors
 
-# setup LSST env.
+# setup LSST env (the LSST stack).
 export WEEKLY='{lsst_version}'
 source /cvmfs/sw.lsst.eu/linux-x86_64/lsst_distrib/${WEEKLY}/loadLSST.bash
 setup lsst_distrib
 
 # setup PanDA env.
+# If the stack is not set up elsewhere first, this setup will be insufficient.
 latest_panda=$(ls -td /cvmfs/sw.lsst.eu/linux-x86_64/panda_env/v* | head -1)
 source ${latest_panda}/setup_panda_usdf.sh
 
